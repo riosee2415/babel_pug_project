5fe1a7792cfd0f32c63f958afb656ff37e76cdcb
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-02-09 12:26:00 +0900

    settings babel, gitignore, test main page view

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.listen(PORT, () => {
+  console.log(`🍀 ${PORT} SERVER START!`);
+});
